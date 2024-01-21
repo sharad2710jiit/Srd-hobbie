20a83205edd2003f51adf2e22ab9b6554888a1a6
sharad <sharad13102185@gmail.com>
2024-01-21 19:09:46 +0530

    add all changes

--- spring-backend/src/main/java/backend/hobbiebackend/HobbieBackendApplication.java
+++ spring-backend/src/main/java/backend/hobbiebackend/HobbieBackendApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
 public class HobbieBackendApplication {
